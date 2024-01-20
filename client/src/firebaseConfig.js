44cea95287ae32a1ab4da1ce1585019ab46bc0ae
Niladri Adhikary <rickyrickado14@gmail.com>
2024-01-20 13:12:55 +0530

    [usj

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,18 +1,23 @@
-import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
-const firebaseConfig =
-{
+const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const storage = firebase.storage();
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const usersCollection = collection(db, "users");
 
